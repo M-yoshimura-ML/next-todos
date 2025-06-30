bb53507ad807eb2b5010da0f676bbff654d3fe6d
M-yoshimura-ML <masadrst@gmail.com>
2025-06-30 09:29:10 +0900

    Modify GET method

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -7,7 +7,17 @@ const DB_NAME = 'next-todos';
 const COLLECTION_NAME = 'todos';
 
 export async function GET() {
-  return NextResponse.json(mockTodos)
+  const client = await mongoDBClient;
+  const db = client.db(DB_NAME);
+
+  const todos = await db.collection(COLLECTION_NAME).find().toArray();
+
+  const todosWithId = todos.map((todo) => ({
+    _id: todo._id.toString(),
+    text: todo.text,
+    completed: todo.completed
+  }));
+  return NextResponse.json(todosWithId);
 }
 
 export async function POST(req: Request) {
