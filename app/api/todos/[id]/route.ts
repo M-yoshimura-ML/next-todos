49870e633d4644c50f601b0c7469e000583041e7
M-yoshimura-ML <masadrst@gmail.com>
2025-06-30 09:38:13 +0900

    Modify PUT & DEELTE methods

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -1,19 +1,30 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { toggleTodo, deleteTodo } from '@/lib/mockData';
+import mongoDBClient from '@/lib/mongodb';
+import { ObjectId } from 'mongodb';
 
-// let mockTodos = [
-//   { _id: '1', text: 'go shopping', completed: false },
-//   { _id: '2', text: 'read book', completed: true },
-// ]
+const DB_NAME = 'next-todos';
+const COLLECTION_NAME = 'todos';
 
 export async function PUT(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-  toggleTodo(id);
-  return NextResponse.json({ message: 'updated' })
+  const client = await mongoDBClient;
+  const db = client.db(DB_NAME);
+
+  const todos = db.collection(COLLECTION_NAME);
+  const todo = await todos.findOne({ _id: new ObjectId(id) });
+  if(!todo) return NextResponse.json({ error: 'Not Found' }, { status: 404 });
+  //toggleTodo(id);
+  await todos.updateOne({ _id: new ObjectId(id) }, { $set: { completed: !todo.completed } });
+  return NextResponse.json({ message: 'updated' });
 }
 
 export async function DELETE(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-  deleteTodo(id);
+  const client = await mongoDBClient;
+  const db = client.db(DB_NAME);
+
+  const todos = await db.collection(COLLECTION_NAME).deleteOne({ _id: new ObjectId(id) });
+  //deleteTodo(id);
   return NextResponse.json({ message: 'deleted' });
 }
